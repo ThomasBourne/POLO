cdeceff917580c84c805fe3a49f043d1502532f6
Thomas Bourne <69239859+ThomasBourne@users.noreply.github.com>
2021-02-06 10:57:58 +0000

    Add files via upload

---
+++ POLO/POLO/Run.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace POLO
+{
+    class Run
+    {
+        static void Main(string[] args)
+        {
+            POLO polo = new POLO();
+            //string path C:\Users\Thomas\Desktop\POLO\POLO FILES\Main.p
+            Console.Write("Enter path for main POLO file:\n>>> ");
+            string path = Console.ReadLine();
+            Console.Clear();
+            if (!polo.validPath(path))
+                Environment.Exit(0);
+            string file = File.ReadAllText(path);
+            //make this bit run multiple times for each ;
+            List<string> commands = polo.Command(file);
+            Console.WriteLine($"FILE CONTENTS:\n{file}\n\n\n\n");
+            Console.Write($"OUTPUT:\n");
+            for (int i = 0; i < commands.Count(); i++)
+            {
+                int? output = polo.ExecuteNumeric(commands[i]);
+                if (output == null)
+                    Environment.Exit(0);
+                else
+                {
+                    Console.Write($"{output}\n");
+                }
+            }
+        }
+    }
+}
---
+++ POLO/POLO/Store.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POLO
+{
+    class Store
+    {
+        public static readonly string[] allowedTypes = { "polo", "POLO", "p", "pf" };
+        public static readonly string mathsSymbols = "+-*/%^";
+    }
+}
